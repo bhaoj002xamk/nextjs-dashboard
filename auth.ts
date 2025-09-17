c684b4094bdbea089e6fa9ad6bd609e2e9e5815b
bhaoj002xamk <bhaoj002@edu.xamk.fi>
2025-09-17 21:53:23 +0300

    auth3

--- auth.ts
+++ auth.ts
@@ -6,12 +6,20 @@ import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL_NON_POOLING!, { ssl: 'require' });
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
+    const users = await sql`SELECT * FROM users WHERE email=${email}`;
+    if (users.length === 0) return undefined;
+    const userRow = users[0];
+    const user: User = {
+      id: userRow.id,
+      name: userRow.name,
+      email: userRow.email,
+      password: userRow.password,
+    };
+    return user;
   } catch (error) {
     console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
